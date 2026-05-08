de82e587a3b0b51042e2402e42d95711545f89a8
ozzypawan <pawanpandey1251@gmail.com>
2026-05-09 04:10:53 +0545

    feat: Translations update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-
+export default defineConfig(() => ({
+  base: "/pawanrajpandey.com.np/",
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
